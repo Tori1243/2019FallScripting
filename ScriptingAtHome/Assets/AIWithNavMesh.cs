b50f91acba6cb0dd6f89ac03b874c15e5ccee38f
Unknown <Tori7809@gmail.com>
2019-10-14 16:26:59 -0600

    Action calls and Ai Script

---
+++ ScriptingAtHome/Assets/AIWithNavMesh.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class AIWithNavMesh : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform player;
+    public Transform destination;
+
+    private void Start()
+    {
+        destination = transform;
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        destination = transform;
+    }
+
+    void Update()
+    {
+        agent.destination = player.position;
+    }
+}
---
+++ ScriptingAtHome/Assets/ActionCalls.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionCalls : MonoBehaviour
+{
+
+    public float speed = 1f, rotateSpeed = 10f, scaleSpeed = 0.02f;
+
+    private Vector3 location, rotations, sclaes;
+
+    private List<UnityAction> actions;
+
+    private int i;
+
+    private void Awake()
+    {
+
+        actions = new List<UnityAction> { OnMove, OnRotate, Onscale };
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (i == actions.Count - 1)
+            i = 0;
+        else
+            i++;
+    }
+    private void Update()
+    {
+
+        actions[i]();
+
+    }
+
+    private void OnMove()
+    {
+        location.x = speed * Time.deltaTime;
+        transform.Translate(location);
+    }
+
+    private void OnRotate()
+    {
+        rotations.y = rotateSpeed * Time.deltaTime;
+
+        transform.Rotate(rotations);
+    }
+
+}
+
+
+
+
