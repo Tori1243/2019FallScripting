2dac84d1bcf8e3495f2833561486004b8e741ad7
Unknown <Tori7809@gmail.com>
2019-10-22 11:55:05 -0600

    Lots and Lots of scripts
    Scripts that we've done in class applied to my at home scene

---
+++ ScriptingAtHome/Assets/Scripts/MonoEventBehavior.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MonoEventBehaviours : MonoBehaviour
+{
+    public UnityEvent StartEvent;
+    void Start()
+    {
+        StartEvent.Invoke();
+    }
+}
