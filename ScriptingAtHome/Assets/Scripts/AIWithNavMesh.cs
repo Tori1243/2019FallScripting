cd69d6032dc4b8747feca40b176acb9622b0967c
Unknown <Tori7809@gmail.com>
2019-10-22 12:16:03 -0600

    Interface Scripts

--- ScriptingAtHome/Assets/Scripts/AIWithNavMesh.cs
+++ ScriptingAtHome/Assets/Scripts/AIWithNavMesh.cs
@@ -1,28 +1,49 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(NavMeshAgent))]
-
-public class AIWithNavMesh : MonoBehaviour
+public class AiWithNavMesh : MonoBehaviour, IRun, ITest
 {
     private NavMeshAgent agent;
     public Transform player;
-    public Transform destination;
 
-    private void Start()
+    public Transform destination;
+    void Start()
     {
         destination = transform;
         agent = GetComponent<NavMeshAgent>();
-
     }
 
     private void OnTriggerEnter(Collider other)
+    {
+        destination = player;
+    }
+
+    private void OnTriggerExit(Collider other)
     {
         destination = transform;
     }
 
     void Update()
     {
-        agent.destination = player.position;
+        agent.destination = destination.position;
+    }
+
+    public float Speed { get; set; }
+
+    public void Run()
+    {
+
+    }
+
+    public void Run(float f)
+    {
+
+    }
+
+    public void Test()
+    {
+
     }
 }
